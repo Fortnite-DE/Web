7c88607cd43e9de23efa2bd674e359c53180e9f6
Lucas Hardt <Luc1412.lh@gmail.com>
2022-10-13 01:51:49 +0200

    First Fortnite home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif'],
+    },
+    animation: {
+      'arrow-colors-1': 'color_anim 1s infinite 0.6s',
+      'arrow-colors-2': 'color_anim 1s infinite 0.4s',
+      'arrow-colors-3': 'color_anim 1s infinite 0.2s',
+    },
+    keyframes: {
+        color_anim: {
+          '0%, 100%': { fill: 'white' },
+          '50%': { fill: 'black' },
+        }
+    },
     extend: {},
   },
   plugins: [],
