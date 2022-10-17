a0117c1ebf0d62fbbaa9a8fe801a8cf495440092
Lucas Hardt <Luc1412.lh@gmail.com>
2022-10-17 15:43:31 +0200

    First verify success implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,19 +9,25 @@ module.exports = {
       'poppins': ['Poppins', 'sans-serif'],
     },
     backgroundImage: {
-      'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      'verify-confirm': 'radial-gradient(circle at 50% -50%, hsl(87.86, 96.08%, 60%) 0%, hsla(87.86, 96.08%, 60%, 0.935) 0.4%,hsla(87.86, 96.08%, 60%, 0.861) 1.6%,hsla(87.86, 96.08%, 60%, 0.781) 3.7%,hsla(87.86, 96.08%, 60%, 0.607) 10.4%,hsla(87.86, 96.08%, 60%, 0.518) 15%,hsla(87.86, 96.08%, 60%, 0.43) 20.6%,hsla(87.86, 96.08%, 60%, 0.346) 27.1%,hsla(87.86, 96.08%, 60%, 0.266) 34.6%,hsla(87.86, 96.08%, 60%, 0.193) 43%,hsla(87.86, 96.08%, 60%, 0.128) 52.3%,hsla(87.86, 96.08%, 60%, 0.075) 62.7%,hsla(87.86, 96.08%, 60%, 0.035) 74.1%,hsla(87.86, 96.08%, 60%, 0.009) 86.5%,hsla(87.86, 96.08%, 60%, 0) 100%),url(/images/verified.webp)',
       'fortnite': "url('/images/fortnite.webp')",
       'fortnite-responsive': "url('/images/fortnite_responsive.webp')",
+      'elements': "url('/images/elements.svg')"
     },
     animation: {
       'arrow-colors-1': 'color_anim 1s infinite 0.6s',
       'arrow-colors-2': 'color_anim 1s infinite 0.4s',
       'arrow-colors-3': 'color_anim 1s infinite 0.2s',
+      'verified-scale': 'scale 3s ease-in-out'
     },
     keyframes: {
         color_anim: {
           '0%, 100%': { fill: 'white' },
           '50%': { fill: 'black' },
+        },
+        scale: {
+          '0%, 35%': { transform: 'scale(0.0)', opacity: '0%' },
+          '100%': { transform: 'scale(1.0)', opacity: '100%' }
         }
     },
     extend: {},
